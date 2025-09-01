45f26e3aa0f5181658bb2f28df86d0494150c311
Daniel99177 <esteban.uchima@gmail.com>
2025-09-01 16:20:50 -0500

    feat(router) : Se agrego ruta para componente registro

--- src/components/router/AppRouter.tsx
+++ src/components/router/AppRouter.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../home/Home";
+import RegisterForm from "../register/RegisterForm";
 
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Home />} />
+
+        <Route path="/register" element={<RegisterForm />} />
+
       </Routes>
     </BrowserRouter>
   );
